8e39bc1f063b93957e0433fa855822c2d22250f1
CharlesOkechukwu <realcharlieok@gmail.com>
2024-05-21 02:14:56 +0100

    add method to return appropriate pages of a dataset

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -24,19 +24,19 @@ class Server:
             self.__dataset = dataset[1:]
 
         return self.__dataset
-    
+
     @staticmethod
     def index_range(page, page_size):
-         """returns the start and end indexes of a page"""
-         end_index = page * page_size
-         start_index = end_index - page_size
-         return (start_index, end_index)
+        """returns the start and end indexes of a page"""
+        end_index = page * page_size
+        start_index = end_index - page_size
+        return (start_index, end_index)
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-            """returns the appropriate page of a dataset"""
-            assert type(page) == int and page > 0
-            assert type(page_size) == int and page_size > 0
-            indexes = self.index_range(page, page_size)
-            start_index = indexes[0]
-            end_index = indexes[1]
-            return self.dataset()[start_index:end_index]
+        """returns the appropriate page of a dataset"""
+        assert type(page) is int and page > 0
+        assert type(page_size) is int and page_size > 0
+        indexes = self.index_range(page, page_size)
+        start_index = indexes[0]
+        end_index = indexes[1]
+        return self.dataset()[start_index:end_index]
