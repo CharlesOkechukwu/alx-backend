8c5bff935597d1643ddbc2ec1c7e3c5bd1ff6edd
CharlesOkechukwu <realcharlieok@gmail.com>
2024-05-22 12:59:42 +0100

    implemeent put and get method  for basic caching class

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Class module for basic cache implementation"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """a basic cache implemetation class that inherits from
+    BaseCaching"""
+    def put(self, key, item):
+        """add key/value pair to cache"""
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """return value in self.cache_data for a given key"""
+        if key is not None:
+            return self.cache_data.get(key)
+        return None
