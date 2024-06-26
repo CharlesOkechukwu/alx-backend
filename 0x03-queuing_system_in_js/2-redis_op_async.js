b72a568be22810b9478aa16683d6c4d018c43e0f
CharlesOkechukwu <realcharlieok@gmail.com>
2024-06-26 13:40:56 +0000

    set and dsiplay values

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,30 @@
+import { createClient } from 'redis';
+
+const client = createClient();
+client.on('error', (err) => {
+	  console.log('Redis client not connected to the server:', err.toString());
+});
+
+client.on('connect', () => {
+	  console.log('Redis client connected to the server');
+});
+
+function setNewSchool(schoolName, value) {
+  client.SET(schoolName, value, print);
+}
+
+async function displaySchoolValue(schoolName) {
+  const display = promisify(client.GET).bind(client);
+  try {
+    const value = await display(schoolName);
+    console.log(value);
+  } catch (error) {
+    console.log(error.toString());
+  }
+}
+
+(async () => {
+  await displaySchoolValue('Holberton');
+  setNewSchool('HolbertonSanFrancisco', '100');
+  await displaySchoolValue('HolbertonSanFrancisco');
+})();
